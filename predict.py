305ac5360375370766720a61bd1845a0f78b2038
Pablo Llueca <pau@crowdai.com>
2019-01-24 17:36:23 -0800

    added CSVlogger that logs epoch elapsed time, aded requirements.txt and job script

--- predict.py
+++ predict.py
@@ -15,7 +15,9 @@ def predict(x, model, patch_sz=160, n_classes=5):
     npatches_horizontal = math.ceil(img_width / patch_sz)
     extended_height = patch_sz * npatches_vertical
     extended_width = patch_sz * npatches_horizontal
-    ext_x = np.zeros(shape=(extended_height, extended_width, n_channels), dtype=np.float32)
+    ext_x = np.zeros(
+        shape=(extended_height, extended_width, n_channels), dtype=np.float32
+    )
     # fill extended image with mirrors:
     ext_x[:img_height, :img_width, :] = x
     for i in range(img_height, extended_height):
@@ -34,7 +36,9 @@ def predict(x, model, patch_sz=160, n_classes=5):
     patches_array = np.asarray(patches_list)
     # predictions:
     patches_predict = model.predict(patches_array, batch_size=4)
-    prediction = np.zeros(shape=(extended_height, extended_width, n_classes), dtype=np.float32)
+    prediction = np.zeros(
+        shape=(extended_height, extended_width, n_classes), dtype=np.float32
+    )
     for k in range(patches_predict.shape[0]):
         i = k // npatches_horizontal
         j = k % npatches_vertical
@@ -48,72 +52,88 @@ def picture_from_mask(mask, threshold=0):
     colors = {
         0: [150, 150, 150],  # Buildings
         1: [223, 194, 125],  # Roads & Tracks
-        2: [27, 120, 55],    # Trees
+        2: [27, 120, 55],  # Trees
         3: [166, 219, 160],  # Crops
-        4: [116, 173, 209]   # Water
+        4: [116, 173, 209],  # Water
     }
-    z_order = {
-        1: 3,
-        2: 4,
-        3: 0,
-        4: 1,
-        5: 2
-    }
-    pict = 255*np.ones(shape=(3, mask.shape[1], mask.shape[2]), dtype=np.uint8)
+    z_order = {1: 3, 2: 4, 3: 0, 4: 1, 5: 2}
+    pict = 255 * np.ones(shape=(3, mask.shape[1], mask.shape[2]), dtype=np.uint8)
     for i in range(1, 6):
         cl = z_order[i]
         for ch in range(3):
-            pict[ch,:,:][mask[cl,:,:] > threshold] = colors[cl][ch]
+            pict[ch, :, :][mask[cl, :, :] > threshold] = colors[cl][ch]
     return pict
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     model = get_model()
     model.load_weights(weights_path)
-    test_id = 'test'
-    img = normalize(tiff.imread('data/mband/{}.tif'.format(test_id)).transpose([1,2,0]))   # make channels last
+    test_id = "test"
+    img = normalize(
+        tiff.imread("data/mband/{}.tif".format(test_id)).transpose([1, 2, 0])
+    )  # make channels last
 
     for i in range(7):
         if i == 0:  # reverse first dimension
-            mymat = predict(img[::-1,:,:], model, patch_sz=PATCH_SZ, n_classes=N_CLASSES).transpose([2,0,1])
-            #print(mymat[0][0][0], mymat[3][12][13])
-            print("Case 1",img.shape, mymat.shape)
-        elif i == 1:    # reverse second dimension
-            temp = predict(img[:,::-1,:], model, patch_sz=PATCH_SZ, n_classes=N_CLASSES).transpose([2,0,1])
-            #print(temp[0][0][0], temp[3][12][13])
+            mymat = predict(
+                img[::-1, :, :], model, patch_sz=PATCH_SZ, n_classes=N_CLASSES
+            ).transpose([2, 0, 1])
+            # print(mymat[0][0][0], mymat[3][12][13])
+            print("Case 1", img.shape, mymat.shape)
+        elif i == 1:  # reverse second dimension
+            temp = predict(
+                img[:, ::-1, :], model, patch_sz=PATCH_SZ, n_classes=N_CLASSES
+            ).transpose([2, 0, 1])
+            # print(temp[0][0][0], temp[3][12][13])
             print("Case 2", temp.shape, mymat.shape)
-            mymat = np.mean( np.array([ temp[:,::-1,:], mymat ]), axis=0 )
-        elif i == 2:    # transpose(interchange) first and second dimensions
-            temp = predict(img.transpose([1,0,2]), model, patch_sz=PATCH_SZ, n_classes=N_CLASSES).transpose([2,0,1])
-            #print(temp[0][0][0], temp[3][12][13])
+            mymat = np.mean(np.array([temp[:, ::-1, :], mymat]), axis=0)
+        elif i == 2:  # transpose(interchange) first and second dimensions
+            temp = predict(
+                img.transpose([1, 0, 2]), model, patch_sz=PATCH_SZ, n_classes=N_CLASSES
+            ).transpose([2, 0, 1])
+            # print(temp[0][0][0], temp[3][12][13])
             print("Case 3", temp.shape, mymat.shape)
-            mymat = np.mean( np.array([ temp.transpose(0,2,1), mymat ]), axis=0 )
+            mymat = np.mean(np.array([temp.transpose(0, 2, 1), mymat]), axis=0)
         elif i == 3:
-            temp = predict(np.rot90(img, 1), model, patch_sz=PATCH_SZ, n_classes=N_CLASSES)
-            #print(temp.transpose([2,0,1])[0][0][0], temp.transpose([2,0,1])[3][12][13])
+            temp = predict(
+                np.rot90(img, 1), model, patch_sz=PATCH_SZ, n_classes=N_CLASSES
+            )
+            # print(temp.transpose([2,0,1])[0][0][0], temp.transpose([2,0,1])[3][12][13])
             print("Case 4", temp.shape, mymat.shape)
-            mymat = np.mean( np.array([ np.rot90(temp, -1).transpose([2,0,1]), mymat ]), axis=0 )
+            mymat = np.mean(
+                np.array([np.rot90(temp, -1).transpose([2, 0, 1]), mymat]), axis=0
+            )
         elif i == 4:
-            temp = predict(np.rot90(img,2), model, patch_sz=PATCH_SZ, n_classes=N_CLASSES)
-            #print(temp.transpose([2,0,1])[0][0][0], temp.transpose([2,0,1])[3][12][13])
+            temp = predict(
+                np.rot90(img, 2), model, patch_sz=PATCH_SZ, n_classes=N_CLASSES
+            )
+            # print(temp.transpose([2,0,1])[0][0][0], temp.transpose([2,0,1])[3][12][13])
             print("Case 5", temp.shape, mymat.shape)
-            mymat = np.mean( np.array([ np.rot90(temp,-2).transpose([2,0,1]), mymat ]), axis=0 )
+            mymat = np.mean(
+                np.array([np.rot90(temp, -2).transpose([2, 0, 1]), mymat]), axis=0
+            )
         elif i == 5:
-            temp = predict(np.rot90(img,3), model, patch_sz=PATCH_SZ, n_classes=N_CLASSES)
-            #print(temp.transpose([2,0,1])[0][0][0], temp.transpose([2,0,1])[3][12][13])
+            temp = predict(
+                np.rot90(img, 3), model, patch_sz=PATCH_SZ, n_classes=N_CLASSES
+            )
+            # print(temp.transpose([2,0,1])[0][0][0], temp.transpose([2,0,1])[3][12][13])
             print("Case 6", temp.shape, mymat.shape)
-            mymat = np.mean( np.array([ np.rot90(temp, -3).transpose(2,0,1), mymat ]), axis=0 )
+            mymat = np.mean(
+                np.array([np.rot90(temp, -3).transpose(2, 0, 1), mymat]), axis=0
+            )
         else:
-            temp = predict(img, model, patch_sz=PATCH_SZ, n_classes=N_CLASSES).transpose([2,0,1])
-            #print(temp[0][0][0], temp[3][12][13])
+            temp = predict(
+                img, model, patch_sz=PATCH_SZ, n_classes=N_CLASSES
+            ).transpose([2, 0, 1])
+            # print(temp[0][0][0], temp[3][12][13])
             print("Case 7", temp.shape, mymat.shape)
-            mymat = np.mean( np.array([ temp, mymat ]), axis=0 )
+            mymat = np.mean(np.array([temp, mymat]), axis=0)
 
-    #print(mymat[0][0][0], mymat[3][12][13])
+    # print(mymat[0][0][0], mymat[3][12][13])
     map = picture_from_mask(mymat, 0.5)
-    #mask = predict(img, model, patch_sz=PATCH_SZ, n_classes=N_CLASSES).transpose([2,0,1])  # make channels first
-    #map = picture_from_mask(mask, 0.5)
+    # mask = predict(img, model, patch_sz=PATCH_SZ, n_classes=N_CLASSES).transpose([2,0,1])  # make channels first
+    # map = picture_from_mask(mask, 0.5)
 
-    #tiff.imsave('result.tif', (255*mask).astype('uint8'))
-    tiff.imsave('result.tif', (255*mymat).astype('uint8'))
-    tiff.imsave('map.tif', map)
+    # tiff.imsave('result.tif', (255*mask).astype('uint8'))
+    tiff.imsave("result.tif", (255 * mymat).astype("uint8"))
+    tiff.imsave("map.tif", map)
--- train_unet.py
+++ train_unet.py
@@ -1,3 +1,6 @@
+import time
+import datetime
+
 from unet_model import *
 from gen_patches import *
 
@@ -16,65 +19,111 @@ def normalize(img):
     return x
 
 
-
 N_BANDS = 8
 N_CLASSES = 5  # buildings, roads, trees, crops and water
 CLASS_WEIGHTS = [0.2, 0.3, 0.1, 0.1, 0.3]
 N_EPOCHS = 150
 UPCONV = True
-PATCH_SZ = 160   # should divide by 16
+PATCH_SZ = 160  # should divide by 16
 BATCH_SIZE = 150
 TRAIN_SZ = 4000  # train size
-VAL_SZ = 1000    # validation size
+VAL_SZ = 1000  # validation size
 
 
 def get_model():
-    return unet_model(N_CLASSES, PATCH_SZ, n_channels=N_BANDS, upconv=UPCONV, class_weights=CLASS_WEIGHTS)
+    return unet_model(
+        N_CLASSES,
+        PATCH_SZ,
+        n_channels=N_BANDS,
+        upconv=UPCONV,
+        class_weights=CLASS_WEIGHTS,
+    )
 
 
-weights_path = 'weights'
+weights_path = "weights"
 if not os.path.exists(weights_path):
     os.makedirs(weights_path)
-weights_path += '/unet_weights.hdf5'
+weights_path += "/unet_weights.hdf5"
 
-trainIds = [str(i).zfill(2) for i in range(1, 25)]  # all availiable ids: from "01" to "24"
+trainIds = [
+    str(i).zfill(2) for i in range(1, 25)
+]  # all availiable ids: from "01" to "24"
 
 
-if __name__ == '__main__':
+class CustomCSVLoggerCallback(CSVLogger):
+    """ custom callback that does the same as the original but also records time per epoch """
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.epoch_start_time = time.time()
+        super().on_epoch_begin(epoch, logs)
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        elapsed = time.time() - self.epoch_start_time()
+        logs["elapsed_time"] = elapsed
+        super().on_epoch_end(epoch, logs)
+
+
+if __name__ == "__main__":
     X_DICT_TRAIN = dict()
     Y_DICT_TRAIN = dict()
     X_DICT_VALIDATION = dict()
     Y_DICT_VALIDATION = dict()
 
-    print('Reading images')
+    print("Reading images")
     for img_id in trainIds:
-        img_m = normalize(tiff.imread('./data/mband/{}.tif'.format(img_id)).transpose([1, 2, 0]))
-        mask = tiff.imread('./data/gt_mband/{}.tif'.format(img_id)).transpose([1, 2, 0]) / 255
-        train_xsz = int(3/4 * img_m.shape[0])  # use 75% of image as train and 25% for validation
+        img_m = normalize(
+            tiff.imread("./data/mband/{}.tif".format(img_id)).transpose([1, 2, 0])
+        )
+        mask = (
+            tiff.imread("./data/gt_mband/{}.tif".format(img_id)).transpose([1, 2, 0])
+            / 255
+        )
+        train_xsz = int(
+            3 / 4 * img_m.shape[0]
+        )  # use 75% of image as train and 25% for validation
         X_DICT_TRAIN[img_id] = img_m[:train_xsz, :, :]
         Y_DICT_TRAIN[img_id] = mask[:train_xsz, :, :]
         X_DICT_VALIDATION[img_id] = img_m[train_xsz:, :, :]
         Y_DICT_VALIDATION[img_id] = mask[train_xsz:, :, :]
-        print(img_id + ' read')
-    print('Images were read')
+        print(img_id + " read")
+    print("Images were read")
 
     def train_net():
         print("start train net")
-        x_train, y_train = get_patches(X_DICT_TRAIN, Y_DICT_TRAIN, n_patches=TRAIN_SZ, sz=PATCH_SZ)
-        x_val, y_val = get_patches(X_DICT_VALIDATION, Y_DICT_VALIDATION, n_patches=VAL_SZ, sz=PATCH_SZ)
+        x_train, y_train = get_patches(
+            X_DICT_TRAIN, Y_DICT_TRAIN, n_patches=TRAIN_SZ, sz=PATCH_SZ
+        )
+        x_val, y_val = get_patches(
+            X_DICT_VALIDATION, Y_DICT_VALIDATION, n_patches=VAL_SZ, sz=PATCH_SZ
+        )
         model = get_model()
         if os.path.isfile(weights_path):
             model.load_weights(weights_path)
-        #model_checkpoint = ModelCheckpoint(weights_path, monitor='val_loss', save_weights_only=True, save_best_only=True)
-        #early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='auto')
-        #reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.1, patience=5, min_lr=0.00001)
-        model_checkpoint = ModelCheckpoint(weights_path, monitor='val_loss', save_best_only=True)
-        csv_logger = CSVLogger('log_unet.csv', append=True, separator=';')
-        tensorboard = TensorBoard(log_dir='./tensorboard_unet/', write_graph=True, write_images=True)
-        model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=N_EPOCHS,
-                  verbose=2, shuffle=True,
-                  callbacks=[model_checkpoint, csv_logger, tensorboard],
-                  validation_data=(x_val, y_val))
+        # model_checkpoint = ModelCheckpoint(weights_path, monitor='val_loss', save_weights_only=True, save_best_only=True)
+        # early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='auto')
+        # reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.1, patience=5, min_lr=0.00001)
+        model_checkpoint = ModelCheckpoint(
+            weights_path, monitor="val_loss", save_best_only=True
+        )
+        now = datetime.datetime.now()
+        csvfpath = (
+            f"log_unet_{now.year}-{now.month}-{now.day}-{now.hour}:{now.minute}.csv"
+        )
+        csv_logger = CSVLogger(csvfpath, append=True, separator=";")
+        tensorboard = TensorBoard(
+            log_dir="./tensorboard_unet/", write_graph=True, write_images=True
+        )
+        model.fit(
+            x_train,
+            y_train,
+            batch_size=BATCH_SIZE,
+            epochs=N_EPOCHS,
+            verbose=2,
+            shuffle=True,
+            callbacks=[model_checkpoint, csv_logger, tensorboard],
+            validation_data=(x_val, y_val),
+        )
         return model
 
     train_net()
